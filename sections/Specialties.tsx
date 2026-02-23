1b8c20d2405354a7831feeaba5f5c5ef4aaed9e5
Fernando S. Santos <106186154+Fernando-S-Santos@users.noreply.github.com>
2026-02-23 16:46:15 -0300

    Change header text color to white

--- sections/Specialties.tsx
+++ sections/Specialties.tsx
@@ -9,7 +9,7 @@ export default function Specialties() {
       <FadeIn>
         <div className="max-w-6xl mx-auto">
 
-          <h2 className="text-3xl md:text-4xl font-bold text-center mb-16">
+          <h2 className="text-3xl md:text-4xl text-white font-bold text-center mb-16">
             Especialidades
           </h2>
 
